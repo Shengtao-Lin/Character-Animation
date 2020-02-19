8e33e33f93327be6bab7193520e74f4f7bd9633f
djx0716 <49354879+djx0716@users.noreply.github.com>
2020-02-19 00:29:53 -0500

    Control navmeshObstacle and agent stop naturally

--- Navigation/Assets/Script/AgentController.cs
+++ Navigation/Assets/Script/AgentController.cs
@@ -10,6 +10,8 @@ public class AgentController : MonoBehaviour
     public LayerMask groundLayer;
 
     List<NavMeshAgent> agents = new List<NavMeshAgent>();
+    List<int> priority = new List<int>();
+    private int rand = 99;
 
     private RaycastHit hitPosition;
 
@@ -27,28 +29,36 @@ public class AgentController : MonoBehaviour
                 NavMeshAgent temp_agent = hitPosition.collider.GetComponent<NavMeshAgent>();
 
                 if(!agents.Contains(temp_agent))
-                { 
+                {
+                    while(priority.Contains(rand))
+                    {
+                        rand = (int)Random.Range(0,99);
+                        //Debug.Log("rand = " + rand);
+                    }
+                    priority.Add(rand);
+                    temp_agent.avoidancePriority = rand;
                     temp_render.material = clicked;
                     agents.Add(temp_agent);
                 }
                 else
                 {
+                    Debug.Log("removing " + temp_agent.avoidancePriority);
+                    priority.Remove(temp_agent.avoidancePriority);
                     temp_render.material = new Material(Shader.Find("Diffuse"));
                     agents.Remove(temp_agent);
                 }
-                
             }
         }
         if(Input.GetMouseButtonDown(1))
         {
             // right click: move seletced agents
+            hitPosition = getPointUnderCursor();
             
             foreach(NavMeshAgent agent in agents)
             {
-                agent.destination = getPointUnderCursor().point;
+                agent.destination = hitPosition.point;
             }
         }
-        
     }
 
     
---
+++ Navigation/Assets/Script/NavObstController.cs
@@ -0,0 +1,73 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class NavObstController : MonoBehaviour
+{
+
+    private Transform navObstacles;
+
+    public Material clicked;
+    public Material defaultMaterial;
+
+    private RaycastHit hitPosition;
+
+    public KeyCode forward;
+    public KeyCode backward;
+    public KeyCode left;
+    public KeyCode right;
+
+    public float speed = 2f;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetMouseButtonDown(0))
+        {
+            Physics.Raycast(Camera.main.ScreenPointToRay(Input.mousePosition), out hitPosition, 100);
+            //letf click
+            if(hitPosition.collider.tag == "NavObst")
+            {
+                if(navObstacles == null)
+                {
+                    navObstacles = hitPosition.collider.GetComponent<Transform>();
+                    navObstacles.GetComponent<MeshRenderer>().material = clicked;
+                }
+                else if(navObstacles.name == hitPosition.collider.name)
+                {
+                    //Debug.Log("got'cha bitch!");
+                    navObstacles.GetComponent<MeshRenderer>().material = defaultMaterial;
+                    navObstacles = null;
+                }
+                else
+                {
+                    navObstacles.GetComponent<MeshRenderer>().material = defaultMaterial;
+                    navObstacles = hitPosition.collider.GetComponent<Transform>();
+                    navObstacles.GetComponent<MeshRenderer>().material = clicked;
+                }
+            }
+        }
+
+
+    }
+    private void FixedUpdate() {
+        //handle null navObstacles??
+        if(Input.GetKey(forward))
+        {
+            navObstacles.Translate(Vector3.forward * Time.deltaTime * speed);
+        }
+        if(Input.GetKey(backward))
+        {
+            navObstacles.Translate(Vector3.forward * Time.deltaTime * speed * -1);
+        }
+        if(Input.GetKey(left))
+        {
+            navObstacles.Translate(Vector3.left * Time.deltaTime * speed);
+        }
+        if(Input.GetKey(right))
+        {
+            navObstacles.Translate(Vector3.left * Time.deltaTime * speed * -1);
+        }
+    }
+}
